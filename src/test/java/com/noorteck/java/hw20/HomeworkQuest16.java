f9184b25e4c980460f3e02f106ad1470131697b0
zamonosman <zamonosman@localhost>
2022-01-22 22:36:09 -0500

    New answers to questions 8 and 16 for Day 20

---
+++ src/test/java/com/noorteck/java/hw20/HomeworkQuest16.java
@@ -0,0 +1,46 @@
+package com.noorteck.java.hw20;
+
+public class HomeworkQuest16 {
+	public static void main(String[] args) {
+
+		Homework20Question16 obj = new Homework20Question16();
+
+		String resultOne = obj.concatString("Java", "awesome");
+
+		String resultTwo = obj.concatString("food", "door");
+
+		String resultThree = obj.concatString("java", "training");
+
+		String resultFour = obj.concatString("pro", "ogress");
+
+		System.out.println(resultOne);
+
+		System.out.println(resultTwo);
+
+		System.out.println(resultThree);
+
+		System.out.println(resultFour);
+
+	}
+
+	String concatString(String strOne, String strTwo) {
+
+		String result = "";
+
+		char lastChar = strOne.charAt(strOne.length() - 1);
+		char firstChar = strTwo.charAt(0);
+
+		if (lastChar == firstChar) {
+			result = strOne.concat(strTwo.substring(1));
+		} else {
+
+			result = strOne.concat(strTwo);
+
+		}
+
+		return result;     
+	}
+
+}
+
+
---
+++ src/test/java/com/noorteck/java/hw20/HomeworkQuest8.java
@@ -0,0 +1,34 @@
+package com.noorteck.java.hw20;
+
+public class HomeworkQuest8 {
+	public static void main(String[] args) {
+
+		char resultOne = getCharacter("java training", 2);
+		char resultTwo = getCharacter("java training", 5);
+		char resultThree = getCharacter("java training", 8);
+		char resultFour = getCharacter("java training", 22);
+
+		System.out.println(resultOne);
+		System.out.println(resultTwo);
+		System.out.println(resultThree);
+		System.out.println(resultFour);
+
+	}
+
+	public static char getCharacter(String strOne, int indexNum) {
+
+		char result = ' ';
+
+		if (indexNum > strOne.length()) {
+			result = '?';
+		} else {
+			result = strOne.charAt(indexNum);
+		}
+
+		return result;   
+
+	}
+
+}
+
+
